26de80487bdc4623220d7bd259156621518a4885
Steve Ma <46506416+mastevb@users.noreply.github.com>
2020-06-14 22:43:59 -0700

    Update MySQLDBUtil.java

--- src/main/java/db/MySQLDBUtil.java
+++ src/main/java/db/MySQLDBUtil.java
@@ -5,7 +5,7 @@ public class MySQLDBUtil {
 	private static final String PORT_NUM = "3306";
 	public static final String DB_NAME = "jobDB";
 	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "Jerryandtom2!";
+	private static final String PASSWORD = "***";
 	public static final String URL = "jdbc:mysql://" + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME + "?user=" + USERNAME
 			+ "&password=" + PASSWORD + "&autoReconnect=true&serverTimezone=UTC";
 }
